7518fb2d25dda43fca11a04013bd33f175b45419
Jonatech256 <lutalojonathan@gmail.com>
2022-10-21 19:01:44 +0300

    added objects to java

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ let addeditems = document.getElementById("itemsAdded");
 function runNumber() {
     addeditems.innerHTML = counter = counter + 1; 
 }
+
+const gaming = {
+    name: "gaming laptop",
+    price: 500
+}
+
+const macBook = {
+    name: "Mac Book",
+    price: 550
+}
+
+let addedToCart = []
